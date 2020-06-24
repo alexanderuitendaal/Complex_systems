b20884d1d944cefce81a8fb4bfad3e66662d7c26
AlexUva19 <alexander.uitendaal@gmail.com>
2020-06-24 21:42:25 +0200

    gini over time version

--- wealth.py
+++ wealth.py
@@ -3,18 +3,31 @@ import matplotlib.pyplot as plt
 
 # global variables
 total_pop = 1000
-beta_array = np.linspace(0.1,3,30)
+beta_array = np.linspace(0,3,10)
 market_growth_rate = 0.02
 delta_omega = 0.01
-time_period = 50
+time_period = 10
 
+
+def plot_gini_over_time(gini_dict, beta_array):
+
+    gini_dict = gini_dict
+    # proportions_iterations = iter_list
+
+    for beta in beta_array:
+        plt.plot(gini_dict[beta])
+
+    plt.xlabel('percent of simulation completed')
+    plt.ylabel('gini coeficient over time')
+    plt.title('stability analysis')
+
+    plt.show()
 
 def plot_lorenz(pop, color):
 
     pop_2 = pop.copy()
     pop_2.sort()
     final_wealth = sum(pop_2)
-
     cumulative_wealth = [] # cumulative wealth as a percentage of total wealth
 
     for i in range(101):
@@ -52,9 +65,6 @@ def plot_wealth(pop, color):
     b80 = sum(pop_2[:int(.8 * len(pop))])
     b95= sum(pop_2[:int(.95 * len(pop))])
 
-    # for percentile in np.linspace(0,1,100):
-    #     ind = len(np.where(pop_2 < percentile * total_sum))
-    #     np.append(ind_list, ind)
 
     plt.scatter(20, b20, c = color)
     plt.scatter(40,b40, c = color)
@@ -74,8 +84,6 @@ def plot_wealth(pop, color):
 
 def initial_distribution(type):
 
-    # TODO : initialisation of other distributions such as exponential or pareto
-    # TODO : make realistic input distribition based on real data
 
     if type == 'exponential':
         return np.random.exponential(50, 10)
@@ -87,7 +95,7 @@ def initial_distribution(type):
         return np.random.pareto(.5)
 
     if type == 'uniform':
-        return np.random.uniform(25,75)
+        return np.random.uniform(0,1)
 
 
 def calc_gini(population):
@@ -113,37 +121,37 @@ def wealth_power(wealth, beta):
 
 
 
+
 # create population
 pop = np.empty(total_pop, dtype=object)
 for i in range(len(pop)):
     pop[i] = initial_distribution('uniform')
 
 
-results_dict = {}
-gini_list = []
+gini_dict = {}
 
 
 for beta in beta_array:
-    print('new round')
-
+    # MAKE A GINI OVER TIME
+    gini_over_time = []
 
 # calulate iterations
     max_iter = np.sum(pop) * (np.exp(market_growth_rate* time_period ) -1)/delta_omega
 
-    # plot begin wealth
-
-    # plot_wealth(pop, color = 'b')
-    # plot_lorenz(pop, color = 'r')
-
     # simulation
     chances = wealth_power(pop, beta)
-    check = int(max_iter/10)
+    check = (int(max_iter/10))
     sum_chances = sum(chances)
+
 
     for iter in range(int(max_iter)):
 
-        if iter % check == 0:
-            print(10*iter/max_iter)
+        if  iter % check  == 0:
+            current_gini = calc_gini(pop)
+            gini_over_time.append(current_gini)
+            # iter_list.append(iter/max_iter)
+
+            # print(iter/int(max_iter))
 
         decision = np.random.uniform(0,sum_chances)
         decision_pos, chance_sum = 0, chances[0]
@@ -159,7 +167,16 @@ for beta in beta_array:
         sum_chances += new_wealth - chances[decision_pos]
         chances[decision_pos] = new_wealth
 
+    # print(gini_over_time)
+    gini_dict[beta] = gini_over_time
 
+
+plot_gini_over_time(gini_dict,  beta_array)
+
+# print(beta_array)
+#
+# x = np.linspace(0,1,10)
+# print(x)
 # plot_wealth(pop, color = 'b')
 # plot_lorenz(pop, color = 'r')
 
@@ -168,42 +185,13 @@ for beta in beta_array:
 #     gini_coef_[0] = calc_gini(pop)
 
 
-    results_dict[beta] = pop
-    gini_list.append(calc_gini(pop))
-
-df = pd.DataFrame(results_dict)
-print(gini_list)
-df.to_pickle("./results_test_1.pkl")
-
-# dict = {}
-# for i in range(0,10):
-#     array = np.random.randint(1,101,5)
-#     dict[i] = array
-
-# df = pd.DataFrame(dict)
-#
-# col_1 = df.iloc[:, 4]
-# df.to_pickle("./testpickle_2.pkl")
-# lala = pd.read_pickle("./testpickle_2.pkl")
-#
-# print(lala)
-
-
-# save results of the simulation
+    # results_dict[beta] = pop
+    # gini_list.append(calc_gini(pop))
 
 
 
 
-
-
-
-# for the model, fix time market growth and pop to reasonable amounts (do tests), does the model work?
-# run for 10 beta's, 10 lorenz curves and 10 gini's (enough to see a phase transition)
-
-# hoelang duurt voor t = 50 met 10000 pop, 17:56 start,18.02 (20%), 18.05(30)% ca 30 minuten per run, dus als je snachts laat runnen ong 20 runs possible
-
-
-# next, plot the final gini coeffecient against the beta, and maybe plot a few lorenz curves (the 4 most extremes)
-
-
+# df = pd.DataFrame(results_dict)
+# print(gini_list)
+# df.to_pickle("./results_test_1.pkl")
 
